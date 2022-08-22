a57ab1528a33a1d175dcb24ef197bb3e68e41852
TwoYear98 <1589672301@qq.com>
2022-08-22 16:02:18 +0800

    # 2022年08月22日15:50:34  测试

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ import os
 DEBUG = True
 
 # 读取数据库环境变量
-username = os.environ.get("MYSQL_USERNAME", 'root')
-password = os.environ.get("MYSQL_PASSWORD", 'root')
-db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+# username = os.environ.get("MYSQL_USERNAME", 'root')
+username = "root"
+# password = os.environ.get("MYSQL_PASSWORD", 'root')
+password = "b4fuTZus"
+# db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+db_address = "10.0.224.4:3306"
