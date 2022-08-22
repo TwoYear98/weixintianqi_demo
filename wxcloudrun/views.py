05b2fcab244b6ddb4d5481f514c62a5212e71399
TwoYear98 <1589672301@qq.com>
2022-08-22 17:35:07 +0800

    # 2022年08月22日15:50:34  测试

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,82 +1,67 @@
-from datetime import datetime
-from flask import render_template, request
+import time
+from flask import render_template, request, abort
 from run import app
-from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
-from wxcloudrun.model import Counters
+# from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
+# from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+import xmltodict
 
 
-@app.route('/')
-def index():
-    """
-    :return: 返回index页面
-    """
-    return render_template('index.html')
-
-
-@app.route('/api/count', methods=['POST'])
-def count():
-    """
-    :return:计数结果/清除结果
-    """
-
-    # 获取请求体参数
-    params = request.get_json()
-
-    # 检查action参数
-    if 'action' not in params:
-        return make_err_response('缺少action参数')
-
-    # 按照不同的action的值，进行不同的操作
-    action = params['action']
-
-    # 执行自增操作
-    if action == 'inc':
-        counter = query_counterbyid(1)
-        if counter is None:
-            counter = Counters()
-            counter.id = 1
-            counter.count = 1
-            counter.created_at = datetime.now()
-            counter.updated_at = datetime.now()
-            insert_counter(counter)
-        else:
-            counter.id = 1
-            counter.count += 1
-            counter.updated_at = datetime.now()
-            update_counterbyid(counter)
-        return make_succ_response(counter.count)
-
-    # 执行清0操作
-    elif action == 'clear':
-        delete_counterbyid(1)
-        return make_succ_empty_response()
-
-    # action参数错误
-    else:
-        return make_err_response('action参数错误')
-
-
-@app.route('/api/count', methods=['GET'])
-def get_count():
-    """
-    :return: 计数的值
-    """
-    counter = Counters.query.filter(Counters.id == 1).first()
-    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
-
-
-@app.route("/api/get_message", methods=['POST'])
+@app.route("/api/get_message", methods=['POST', 'GET'])
 def get_message():
     """
     :return: 返回消息
     """
-    print(request.args.to_dict())
-    print(request.form.to_dict())
-    print(request.data)
-    data = request.form.get("data")
-    print(data)
-    # data = request.get_json()
-    print("data")
-    data = "Success"
-    return make_succ_response(data)
+    signature = request.args.get('signature')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+    if request.method == 'GET':
+        # 第一次接入服务器
+        echostr = request.args.get('echostr')
+        if not echostr:
+            abort(403)
+        else:
+            return echostr
+    elif request.method == 'POST':
+        # 表示微信服务器转发消息到本地服务器
+        xml_str = request.data
+        # print(xml_str)
+        if not xml_str:
+            return abort(403)
+        # 对xml字符串进行解析
+        xml_dict = xmltodict.parse(xml_str)
+        xml_dict1 = xml_dict.get('xml')
+        # print(xml_dict1)
+        # 提取消息类型
+        msg_type = xml_dict1.get('MsgType')
+
+        # print(msg_type)
+        if msg_type == 'text':
+            # 这是文本消息
+            # 构造返回值，由为微信服务器回复消息
+            # 重点：以下参数值一个不能少，一个字母不能错，大小写不能错，键名必须完全一样
+            resp_dict = {
+                "xml": {
+                    'ToUserName': xml_dict1.get('FromUserName'),
+                    'FromUserName': xml_dict1.get('ToUserName'),
+                    'CreateTime': int(time.time()),
+                    'MsgType': 'text',
+                    'Content': xml_dict1.get('Content'),
+                }
+            }
+            # print(resp_dict)
+        else:
+            resp_dict = {
+                "xml": {
+                    'ToUserName': xml_dict1.get('FromUserName'),
+                    'FromUserName': xml_dict1.get('ToUserName'),
+                    'CreateTime': int(time.time()),
+                    'MsgType': 'text',
+                    'Content': "我们收到您的消息，谢谢您"
+                }
+            }
+            # 将字典转为xml字符串
+        resp_xml_str = xmltodict.unparse(resp_dict)
+        print(resp_xml_str)
+        # # 返回消息字符串
+        return resp_xml_str
